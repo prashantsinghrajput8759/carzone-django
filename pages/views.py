cd0764c9934da580fc801e9adb39e18927b6e550
prashant <singhprashant8759@gmail.com>
2022-06-10 15:50:41 +0530

    #2

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'../templates/pages/home.html')
+
+def about(request):
+    return render(request,'../templates/pages/about.html')
+
+def services(request):
+    return render(request,'../templates/pages/services.html')
+
+def cars(request):
+    return render(request,'../templates/pages/cars.html')
+
+def contact(request):
+    return render(request,'../templates/pages/contact.html')
