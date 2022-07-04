2717e925ee33f59ae3caf57844b62fc350d77071
prashant <singhprashant8759@gmail.com>
2022-07-04 16:16:17 +0530

    search

--- cars/urls.py
+++ cars/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
         path('',views.cars,name='cars'),
+        path('<int:id>',views.cars_detail,name='cars_detail'),
+        path('search',views.search,name='search'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -7,10 +7,19 @@ def home(request):
     team=Team.objects.all()
     featured_cars=car.objects.order_by('created_date').filter(is_featured=True)
     all_cars=car.objects.order_by('created_date')
+    # search_fields=car.objects.values('model','city','year','body_style')
+    model_search=car.objects.values_list('model',flat=True).distinct()
+    city_search=car.objects.values_list('city',flat=True).distinct()
+    year_search=car.objects.values_list('year',flat=True).distinct()
+    body_style_search=car.objects.values_list('body_style',flat=True).distinct()
     data={
         'teams':team,
         'featured_cars':featured_cars,
         'all_cars': all_cars,
+        'model_search':model_search,
+        'city_search':city_search,
+        'year_search':year_search,
+        'body_style_search':body_style_search,
     }
     return render(request,'../templates/pages/home.html',data)
 
